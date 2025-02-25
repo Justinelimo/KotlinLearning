b9b64708e93923abb7ebad2abe7cb7b1d5f1d89a
justine limo <justinelimoco98@gmail.com>
2025-02-25 09:21:06 +0300

    Learned about default params and tried to promt one similar to gmail app

--- app/src/main/java/slee/kotlin/myfirstandroidapp/Hello.kt
+++ app/src/main/java/slee/kotlin/myfirstandroidapp/Hello.kt
@@ -1,5 +1,14 @@
 package slee.kotlin.myfirstandroidapp
 
-fun greetings(){
-    println("Hello Slee .......")
+
+fun main(){
+
+    val operatingSystem = "ChromeOS"
+    val emailId = "slee98@gmail.com"
+
+    println(displayAlertMessage(operatingSystem , emailId))
 }
+
+    fun displayAlertMessage(operatingSystem:String , emailId:String): String{
+        return "There is a signin request on $operatingSystem for your google acc $emailId"
+    }
